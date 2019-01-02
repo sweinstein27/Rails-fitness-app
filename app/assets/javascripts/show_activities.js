ec2f0c43746626cabe1d603d1ad3b7e7cfc726e9
sweinstein27 <sweinstein27@gmail.com>
2019-01-02 08:58:23 -0800

    added get function to next script

--- app/assets/javascripts/show_activities.js
+++ app/assets/javascripts/show_activities.js
@@ -1,9 +1,9 @@
-$(document).ready(function(){
+$(function () {
     $( "#next" ).click(function(e){
         e.preventDefault()
         var nextId = parseInt($("#next").attr("data-id")) + 1;
         $.get("/activity_datum/" + nextId, function(data) {
-            console.log(data)
+           $(".dataName").text(data["name"]);
         })
        
     });

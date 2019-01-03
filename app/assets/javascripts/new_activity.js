267f2bae76f7b4591944370cea13a8328f7e263e
sweinstein27 <sweinstein27@gmail.com>
2019-01-03 07:29:50 -0800

    succesfully appended new form on user show page and hijacked submit

--- app/assets/javascripts/new_activity.js
+++ app/assets/javascripts/new_activity.js
@@ -1,8 +1,28 @@
 $(document).ready(function(){
     $( "#new_activity" ).click(function(e){
-        debugger
-        e.stopImmediatePropagation();
-        alert("Not Today Satan");
-        return false;
+        e.preventDefault();
+        $.ajax({
+            type: "GET",
+            url: "/entries/new",
+            dataType: "html",
+            success: function(data) {
+                console.log(data)
+                $("#new-form").html(data);
+            },
+            error: function(response) {
+                console.log(response)
+                alert("oops!")
+            }
+        })
+    });
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        alert("stopped");
+
+        var values = $(this).serialize();
+        
+        var posting = $.post(this.action, values);
     });
   });
+
